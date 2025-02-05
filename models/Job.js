7e800c06a9b0ea57917c172c8d3ca19f9eac837a
YuvrajKathpalia <yuvraj.kathpalia20@gmail.com>
2025-02-05 18:16:43 +0530

    new

--- models/Job.js
+++ models/Job.js
@@ -1,33 +1,28 @@
 const mongoose = require('mongoose');
 
 const jobSchema = new mongoose.Schema({
-
   title: { type: String, required: true },
   description: { type: String, required: true },
-  location: { type: String, required: true },
+  location: { type: String},
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true }, 
-  employer: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  companyName: { type: String, required: true }, 
-  salary: { type: Number, required: true }, 
+  employer: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  companyName: { type: String },  
+  salary: { type: Number },  
   skillsRequired: [String],
 
   employmentType: {
     type: String,
-    enum: ['full-time', 'part-time', 'internship', 'any'], 
+    enum: ['full-time', 'part-time', 'internship', 'any'],
+    default: 'any' 
   },
   workExperience: {
     type: String,
-    enum: ['0-1 years', '1-3 years', '3-5 years', 'more than 5 years', 'any'], 
-    default: 'any'
+    enum: ['0-1 years', '1-3 years', '3-5 years', '5 years','more than 5 years', 'any'], 
+    default: 'any' 
   },
-  dateOfPosting: { type: Date, default: Date.now } 
+  dateOfPosting: { type: Date, default: Date.now }
 });
-
-
-// Create a text index on ttile and description fields..
 
 jobSchema.index({ title: 'text', description: 'text' });
 
 module.exports = mongoose.model('Job', jobSchema);
-
-
