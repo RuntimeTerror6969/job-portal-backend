c97ddfbb38a54b380f43d0c77b90eec0803db704
adarsh <aadarsh.22.2003@gmail.com>
2025-02-08 18:13:36 +0530

    Fix job apply

--- models/Application.js
+++ models/Application.js
@@ -1,12 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const applicationSchema = new mongoose.Schema({
-    
-  candidate: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job', required: true },
-  resume: { type: String, required: true },
-  status: { type: String, enum: ['pending', 'reviewed', 'accepted', 'rejected'], default: 'pending' },
-  appliedDate: { type: Date, default: Date.now }
+  candidate: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  job: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
+  resume: { type: String, required: true }, // now stores the Google Drive URL
+  status: {
+    type: String,
+    enum: ["pending", "reviewed", "accepted", "rejected"],
+    default: "pending",
+  },
+  appliedDate: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Application', applicationSchema);
+module.exports = mongoose.model("Application", applicationSchema);
